c11f47e04eaafde4d8d293d54ff91fbbfe6ba607
Romario Negreiros <nromario482@gmail.com>
2024-02-03 18:10:24 -0300

    Add export aliases

---
+++ src/components/svg/index.ts
@@ -0,0 +1,8 @@
+export { default as AboutMeIcon } from "./AboutMeIcon";
+export { default as BlogIcon } from "./BlogIcon";
+export { default as ClearIcon } from "./ClearIcon";
+export { default as ContactIcon } from "./ContactIcon";
+export { default as LogoIcon } from "./LogoIcon";
+export { default as ProjectsIcon } from "./ProjectsIcon";
+export { default as SearchIcon } from "./SearchIcon";
+export { default as StartIcon } from "./StartIcon";
