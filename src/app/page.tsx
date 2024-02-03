94c7778865fb22f05749bd138eccb1a5bf88cb2f
Romario Negreiros <nromario482@gmail.com>
2024-02-03 18:48:52 -0300

    Fix linting errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Header } from "@/components";
 import styles from "./page.module.css";
 
 export default function Home() {
-
   return (
     <main className={styles.container}>
       <Header />
